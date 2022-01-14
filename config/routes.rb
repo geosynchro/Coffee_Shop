7ed85c45a7f9c8961adb20d6d04c356b296b3357
Nicholas Fagner <nicholasfagner@gmail.com>
2022-01-14 10:33:42 -0700

    Completed Rails Resource Generation, associations and schema built

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :reviews
+  resources :carts
+  resources :items
+  resources :comments
+  resources :blogs
+  resources :item_categories
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
