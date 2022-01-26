5e3586c197864d01a18391cf6cd024603236555f
Nicholas Fagner <nicholasfagner@gmail.com>
2022-01-26 15:35:56 -0600

    seed data for heroku attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :carts, dependent: :destroy
-    has_many :items, through: :carts
+    has_many :items, through: :carts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :blogs, dependent: :destroy
     has_many :reviews, dependent: :destroy
